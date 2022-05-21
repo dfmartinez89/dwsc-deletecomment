3beaf5e50972ec5110fac5140d5b7566742c7a35
thywillbedone <dfmartinez8909@gmail.com>
2022-05-21 16:10:23 +0200

    first commit

---
+++ src/main/java/dwsc/proyecto/uncommentmovie/UncommentmovieApplication.java
@@ -0,0 +1,13 @@
+package dwsc.proyecto.uncommentmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UncommentmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UncommentmovieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dwsc/proyecto/uncommentmovie/UncommentmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package dwsc.proyecto.uncommentmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UncommentmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
